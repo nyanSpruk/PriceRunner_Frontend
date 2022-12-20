13541b46acc86713ad2ba9044e33f520fc0e4870
NyanSpruk <slimenian@gmail.com>
2022-12-20 18:31:55 +0100

    Added test code for fetching from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Item } from './classes/item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'PriceRunner_Frontend';
+
+  constructor(private http: HttpClient) {}
+  izdelki: Item[] = [];
+  ngOnInit() {
+    this.http
+      .get<Item[]>('http://localhost:8080/v1/products')
+      .subscribe((data) => {
+        console.log(data);
+        data.forEach((element) => {
+          console.log(element);
+          this.izdelki.push(element);
+        });
+      });
+  }
 }
