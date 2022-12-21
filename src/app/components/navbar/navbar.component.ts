801730b687395e8169bacf5a9362ccfc60091c20
NyanSpruk <slimenian@gmail.com>
2022-12-21 10:35:32 +0100

    Correct icons

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faRightToBracket, faUser } from '@fortawesome/free-solid-svg-icons';
 @Component({
-    selector: 'navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss'],
+  selector: 'navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-    constructor() {}
+  constructor() {}
 
-    ngOnInit(): void {}
+  ngOnInit(): void {}
 
-    pageTitle: string = 'Price Runner';
+  pageTitle: string = 'Price Runner';
 }
