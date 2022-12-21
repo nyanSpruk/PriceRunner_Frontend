40843c4172ed521b0bdca0c15faf99d0e38e96d6
NyanSpruk <slimenian@gmail.com>
2022-12-21 13:26:04 +0100

    Added APi call to create product

--- src/app/screens/create-item/create-item.component.ts
+++ src/app/screens/create-item/create-item.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { Item } from 'src/app/classes/item';
 
 @Component({
   selector: 'create-item',
@@ -6,15 +8,15 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./create-item.component.scss'],
 })
 export class CreateItemComponent implements OnInit {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
-  public name: String = '';
-  public price: Number = 0;
-  public description: String = '';
+  public name: string = '';
+  public price: number = 0;
+  public description: string = '';
 
-  public isInputValid: Boolean = false;
+  public isInputValid: boolean = false;
 
   validateInputFields() {
     this.isInputValid =
@@ -25,6 +27,16 @@ export class CreateItemComponent implements OnInit {
   submitForm() {
     // this.isInputValid = this.validateInputFields();
     if (this.isInputValid) {
+      let item: Item = {
+        name: this.name,
+        price: this.price,
+        description: this.description,
+      };
+      this.http
+        .post('http://localhost:8080/v1/products', item)
+        .subscribe((res) => {
+          console.log(res);
+        });
       console.log('submit form');
     }
   }
