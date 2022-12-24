c38b318973288c48c2afd85bf13a67a081e0fd3a
NyanSpruk <slimenian@gmail.com>
2022-12-24 11:06:15 +0100

    Simple login screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './screens/cart/cart.component';
 import { CreateItemComponent } from './screens/create-item/create-item.component';
+import { LoginComponent } from './screens/user/login/login.component';
 
 // Change '' to redirect to products page
 const routes: Routes = [
   { path: '', redirectTo: 'cart', pathMatch: 'full' },
   { path: 'create-item', component: CreateItemComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' },
 ];
 
